795a0523757a6274a139dbc9667af7051f9aa0af
NekoMio <hui@nekomio.com>
2022-06-16 08:03:30 +0800

    change

--- maketraindata.py
+++ maketraindata.py
@@ -5,27 +5,39 @@ import csv
 import glob
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import scale
-dataset_list=['A']
+dataset_list=['A', 'B1', 'B2', 'C', 'D']
+len_list = {
+	'A': 10,
+	'B1': 5,
+	'B2': 4,
+	'C': 10,
+	'D': 10
+}
 ans=[]
 for dataset in dataset_list:
 	dataset_folder = 'reqdata/' + dataset + '/'
 	files = glob.glob(dataset_folder + '*_tag.csv')
 	#cnt=0
-	for file_name in files:
+	for i in range(len(files)):
+		file_name = files[i]
+		print ("Processing file: " + file_name)
+		if (i >= len_list[dataset]):
+			break
 		file=pd.read_csv(file_name)#wxh's csv name
-		feature_file='RequetDataSet-master\\'+dataset+'\\PCAP_FILES\\'+file_name.split('\\')[1][:-15]+'.pcap'#yhy's pcap name
+		feature_file='RequetDataSet-master/'+dataset+'/PCAP_FILES/'+file_name.split('/')[-1][:-15]+'.pcap'#yhy's pcap name
 		feature_class=chunkdetection.ChunkDetection(feature_file)
 		lable_size=file.shape[0]
 		for i in range(0,lable_size,50):
 			feature=feature_class.getFeature(file.iloc[i,0])
 			if not feature:
 				continue
+			feature.append(file_name + "-" + str(file.iloc[i,0]))
 			feature.extend(file.iloc[i,1:])
 			ans.append(feature)
-out_file=open('traindata/train_data.csv','w',newline='')# output   be careful of filename
+out_file=open('traindata/test_data.csv','w',newline='')# output   be careful of filename
 writer=csv.writer(out_file)
 keys=['label'+str(i) for i in range(120)]
-keys.extend(['status','BuffWarning','Resolution'])
+keys.extend(['filename_time', 'status','BuffWarning','Resolution'])
 writer.writerow(keys)
 for i in ans:
 	writer.writerow(i)
--- randomforest.py
+++ randomforest.py
@@ -1,32 +1,43 @@
+from matplotlib.pyplot import text
 import pandas as pd
 import random
 import csv
+import joblib
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import scale
 from sklearn.model_selection import train_test_split
 train_times=10# 在训练集中建立train_times个随机森林，选择训练集中表现最好的用来跑test
 Tree_num=80#随机森林里树的个数
-train_file_name='traindata/train_data.csv'#train文件名
+train_file_name='traindata/train_data_1655324987.csv'#train文件名
 train_file=pd.read_csv(train_file_name)
-test_file_name='traindata/train_data.csv'#test文件名
+test_file_name='traindata/test_data.csv'#test文件名
 test_file=pd.read_csv(test_file_name)
 X_train=train_file.iloc[:,:-3]
-X_test=test_file.iloc[:,:-3]
+X_test=test_file.iloc[:,:-4]
+name_list=["Status", "BuffWarning", "Resolution"]
 for i in range(-3,0):
-	x_train,x_test,y_train,y_test = train_test_split(train_file.iloc[:,:-3],train_file.iloc[:,i:i+1],test_size=0.3,random_state=2022)#test_size：用来控制选择训练集里test_size的数据作为用来评价随机森林时的测试集
+	x_train,x_test,y_train,y_test = train_test_split(train_file.iloc[:,:-3],train_file.iloc[:,i],test_size=0.3,random_state=2022)#test_size：用来控制选择训练集里test_size的数据作为用来评价随机森林时的测试集
 	RF=RandomForestClassifier(n_estimators=Tree_num)
+	# print (train_file.iloc[:,i])
+	# print (x_train.shape, y_train.shape)
 	RF.fit(x_train,y_train)
 	y_pred=RF.predict(x_test)
 	accuracy=accuracy_score(y_pred,y_test)
-	for j in range(train_times):
-		rf=RandomForestClassifier(n_estimators=Tree_num)
-		rf.fit(x_train,y_train)
-		tmp_y_pred=rf.predict(x_test)
-		if accuracy<accuracy_score(tmp_y_pred,y_test):
-			y_pred=tmp_y_pred
-			accuracy=accuracy_score(tmp_y_pred,y_test)
-			RF=rf
-	Y_test=test_file.iloc[:,i:i+1]
+	# print(accuracy)
+	# for j in range(train_times):
+	# 	rf=RandomForestClassifier(n_estimators=Tree_num)
+	# 	rf.fit(x_train,y_train)
+	# 	tmp_y_pred=rf.predict(x_test)
+	# 	if accuracy<accuracy_score(tmp_y_pred,y_test):
+	# 		y_pred=tmp_y_pred
+	# 		accuracy=accuracy_score(tmp_y_pred,y_test)
+	# 		RF=rf
+	Y_test=test_file.iloc[:,i]
 	Y_pred=RF.predict(X_test)
+	joblib.dump(RF, 'traindata/RF_'+name_list[i]+'.pkl', compress=3)
+	df_truth = pd.DataFrame({'id': test_file.iloc[:,-4], 'label': Y_test})
+	df_pred = pd.DataFrame({'id': test_file.iloc[:,-4], 'label': Y_pred})
+	df_truth.to_csv('pred/'+ name_list[i] + "-truth" + '.csv',index=False)
+	df_pred.to_csv('pred/'+ name_list[i] + "-pred" + '.csv',index=False)
 	print(accuracy_score(Y_pred,Y_test))
